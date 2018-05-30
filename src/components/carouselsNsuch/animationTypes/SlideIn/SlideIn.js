0ee84de99ce35268b4cf01119f280e495589081e
Chris <chriswfoster@gmail.com>
2018-05-30 09:16:14 -0500

    Structuring animations

--- src/components/carouselsNsuch/animationTypes/SlideIn/SlideIn.js
+++ src/components/carouselsNsuch/animationTypes/SlideIn/SlideIn.js
@@ -1,31 +1,45 @@
-import React, {Component} from 'react'
+import React, { Component } from "react"
 
-import SlideInImages from './SlideInImages'
+import { slideInRight, slideOutLeft } from 'react-animations'
 
-class SlideIn extends Component{
-    constructor(){
-        super()
-        this.state= {
-            previousImage: "",
-            nextImage: ""
-        }
+import SlideInImages from "./SlideInImages"
+
+class SlideIn extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      previousImage: this.props.imgArray[0],
+      nextImage: "first"
     }
+  }
 
-    imageLooper(){
-        
-    }
+  imageLooper(arr) {
+    let i = 0
+    setInterval(() => {
+      if (i === arr.length - 1) {
+        i = 0
+        this.setState({ nextImage: arr[0], previousImage: arr[i] })
+      } else {
+        i++
+        this.setState({ nextImage: arr[i + 1], previousImage: arr[i] })
+      }
+    }, 1000)
+  }
 
-
-
-render(){
+  render() {
     console.log(this.props)
-    const {styles, imgArray, imgStyles} = this.props
-return(
-<div style={{...styles, display: "inline"}}>
-{imgArray.map(item => <img src={item} style={{...imgStyles}}/>)}
-</div>
-)
-}
+    const { styles, imgArray, imgStyles } = this.props
+    this.imageLooper(imgArray)
+    return (
+      <div style={{ ...styles, display: "inline" }}>
+        <img src={this.state.previousImage} style={{ ...imgStyles }} />
+
+        {this.state.nextImage === "first" ? null : (
+          <img src={this.state.nextImage} style={{ ...imgStyles }} />
+        )}
+      </div>
+    )
+  }
 }
 export default SlideIn
-// 
+//
