5610c2f75dd7140d9609b901c2b24ec3de120be6
Chris <chriswfoster@gmail.com>
2018-05-30 07:06:01 -0500

    Restructing the new idea

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react"
+
+class Home extends Component {
+  constructor() {
+    super()
+    this.state = {
+      images: [
+        "./carouselsNsuch/images/simple1.jpg",
+        "./carouselsNsuch/images/simple2.jpg",
+        "./carouselsNsuch/images/simple3.jpg",
+        "./carouselsNsuch/images/simple4.jpg",
+        "./carouselsNsuch/images/simple5.jpg"
+      ]
+    }
+  }
+
+  render() {
+    return <div />
+  }
+}
+export default Home
--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 
-import Home from './components/Home'
+import Main from './components/Main'
 
 
 export default(
  
     <Switch>
-    <Route exact path="/" component={Home}/>
+    <Route exact path="/" component={Main}/>
     
     
         </Switch>
